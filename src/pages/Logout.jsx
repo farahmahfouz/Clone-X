3354e1f8b50b05c34f8a2f83e3c52b523373d95b
farahmahfouz <farahmahfouz11@gmail.com>
2024-09-11 13:21:42 +0300

    fix responsive

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function Logout() {
   return (
-    <div className="bg-black grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 h-screen">
-      <div className="flex justify-center">
-      <div className=" bg-black items-center justify-center p-10 h-screen text-white md:flex-row lg:flex lg:justify-between w-10/12">
+    <div className="bg-black grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 ">
+      <div className="flex justify-center bg-black md:h-screen">
+      <div className=" bg-black items-center justify-center p-10  text-white md:flex-row lg:flex lg:justify-between w-10/12">
         <div >
           <img className="max-w-full" src="logo.jpeg" alt="logo" />
         </div>
 
-        <div className="lg:flex lg:flex-col h-screen bg-black items-center">
+        <div className="lg:flex lg:flex-col  bg-black items-center">
           <div className="prose lg:prose-xl mb-8 text-center ">
             <h1 className="text-6xl font-bold">Happening now</h1>
             <h5 className="text-4xl font-semibold pt-5">Join today.</h5>
