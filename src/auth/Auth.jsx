26c1ed4436fcc3916bea719efd68e8199c778657
farahmahfouz <farahmahfouz11@gmail.com>
2024-09-11 13:30:37 +0300

    fixed home

--- src/auth/Auth.jsx
+++ src/auth/Auth.jsx
@@ -4,39 +4,20 @@ import PropTypes from "prop-types";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(localStorage.getItem("token"));
-  const [currentUser, setCurrentUser] = useState(() => {
-    const storedUser = localStorage.getItem("currentUser");
-    return storedUser ? storedUser : null;
-  });
+  const [token, setToken] = useState(localStorage.getItem("token")); 
   
-  
-  // const login = (jwtToken) => {
-  //   setToken(jwtToken);
-  //   localStorage.setItem("token", jwtToken); 
-  // };
-  const login = (token, user) => {
+  const login = (token) => {
     setToken(token);
-    setCurrentUser(user);
-    localStorage.setItem("token", token);
-    localStorage.setItem("currentUser", JSON.stringify(user));
+    localStorage.setItem("token", token); 
   };
-
-  // const logout = () => {
-  //   setToken(null);
-  //   localStorage.removeItem("token"); 
-  // };
-
 
   const logout = () => {
     setToken(null);
-    setCurrentUser(null); 
-    localStorage.removeItem("token");
-    localStorage.removeItem("currentUser");
+    localStorage.removeItem("token"); 
   };
 
   return (
-    <AuthContext.Provider value={{ token,currentUser, login, logout }}>
+    <AuthContext.Provider value={{ token, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
